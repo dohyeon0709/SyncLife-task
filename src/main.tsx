824bb7e3e3404d292153679e9f8ce64d0065cc9d
dohyeon0709 <dohy0709@naver.com>
2026-07-06 22:54:02 +0900

    feat : 로드 상태 처리 구현 - TanStack Query 도입, QueryClientProvider 전역 설정 - 로딩/에러(재시도)/빈 상태 3분기 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './styles.css'
 
@@ -12,10 +13,14 @@ async function enableMocking() {
   })
 }
 
+const queryClient = new QueryClient()
+
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>,
   )
 })
